957185c235fd8f84bcf9bc805a46511d9327bc75
francotaind <francotaind00@gmail.com>
2024-11-07 08:26:38 +0300

    minor fixes

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """Password encryption using the crypt module."""
 
+
 import bcrypt
+
 
 def hash_password(password: str) -> str:
     """Hash a password using the bcrypt algorithm."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check if the password matches the hashed password."""
